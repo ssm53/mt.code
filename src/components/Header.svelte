9c1909690602b6c02f2937efbaa736e1b6290fd9
Shaun Menezes <shaunshanil95@gmail.com>
2024-03-29 03:49:03 +0800

    Reverting back

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -3,12 +3,12 @@
 
   let tabs = [
     {name: "Projects", link: '#projects'}, 
-    {name: "About us", link: '#about'}, ]
+    {name: "About me", link: '#about'}, ]
 </script>
 
 <header class={"sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border-b border-solid " + ( y > 0 ? ' py-4 bg-slate-950 border-violet-950' :' py-6 bg-transparent border-transparent')}>
   <h1 class=" font-medium">
-    <b class=" font-bold poppins">MT.</b> CODE
+    <b class=" font-bold poppins">Shaun</b> Shanil Menezes
   </h1>
   <div class=" sm:flex ml-auto pr-4 items-center gap-4 hidden">
     {#each tabs as tab, index}
